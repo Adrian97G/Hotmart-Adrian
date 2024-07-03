3d478dae51c664f044a02161706c5cc73a6399e8
Adrian Ospina <ospinaadrian784@gmail.com>
2024-07-02 22:45:12 -0500

    Add netlify.toml and update Vite configuration for deployment

--- src/components/AboutSection.vue
+++ src/components/AboutSection.vue
@@ -3,11 +3,11 @@
     <video autoplay muted loop class="absolute inset-0 w-full h-full object-cover z-0">
       <source src="../assets/imgs/about-video.mp4" type="video/mp4">
     </video>
-    <div class="flex flex-col items-center justify-center min-h-screen px-8 md:px-16 lg:px-32 mx-auto z-10 pt-10 pb-10">
+    <div class="flex flex-col items-center justify-center max-h-screen px-8 md:px-16 lg:px-32 mx-auto z-10 pt-10 pb-10">
       <div class="bg-teal-400 opacity-70 rounded-md p-8 text-center flex flex-col items-center justify-center">
-        <img src="../assets/imgs/img-Adrian.png" alt="Adrian-img" class="mb-4 w-25">
+        <img src="../assets/imgs/img-Adrian.png" alt="Adrian-img" class="mb-4 img-adrian">
         <div class="flex flex-col items-center justify-center mb-10">
-          <h1 class="text-5xl text-center font-bold mb-4">Sobre mi</h1>
+          <h1 class="text-4xl text-center font-bold mb-4">Sobre mi</h1>
           <p class="text-2xl mb-2 text-center">
             ¡Hola! soy <strong>Adrian Ospina</strong> y me dedico a recomendar los mejores cursos en Hotmart <strong>(Marketing de afiliados).</strong>
             <br>
@@ -31,6 +31,11 @@
   left: 0;
   min-width: 100%;
   min-height: 100%;
+}
+
+.img-adrian {
+  width: 150px;
+  height: auto;
 }
 
 @media (max-width: 768px) {
